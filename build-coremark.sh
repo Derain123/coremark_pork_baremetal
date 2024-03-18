e04a7f29a4090e1959cad7b1acb49ba19238c151
root <root@localhost.localdomain>
2024-03-18 16:36:52 +0800

    change iteration --- output sec

--- build-coremark.sh
+++ build-coremark.sh
@@ -10,7 +10,7 @@ cd $BASEDIR/$CM_FOLDER
 # run the compile
 echo "Start compilation"
 make PORT_DIR=../riscv64 compile
-mv coremark.riscv ../
+mv coremark.riscv ../cm_riscv
 
-make PORT_DIR=../riscv64-baremetal compile ITERATIONS=100
-mv coremark.bare.riscv ../
+make PORT_DIR=../riscv64-baremetal compile ITERATIONS=2
+mv coremark.bare.riscv ../cm_riscv
